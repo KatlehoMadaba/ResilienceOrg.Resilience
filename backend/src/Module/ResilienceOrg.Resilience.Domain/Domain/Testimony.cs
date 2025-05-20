22dc7bcfc2759c989c80911e34b2355dcb72208e
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-20 22:41:58 +0200

    feature:Testimony created

---
+++ backend/src/Module/ResilienceOrg.Resilience.Domain/Domain/Testimony.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities.Auditing;
+using Shesha.Domain;
+
+namespace ResilienceOrg.Resilience.Domain.Domain
+{
+    public class Testimony : FullAuditedEntity<Guid>
+    {
+        public Guid PersonId { get; set; }
+        public Person Person { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<string> Tags { get; set; }
+        public bool IsAnonymous { get; set; }
+    }
+}

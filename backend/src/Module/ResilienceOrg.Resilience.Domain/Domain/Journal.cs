1911127d52134ac049f24a92ad1fb69c1057c01b
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-20 22:40:41 +0200

    feature:Journal Created

---
+++ backend/src/Module/ResilienceOrg.Resilience.Domain/Domain/Journal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities.Auditing;
+using Shesha.Domain;
+
+namespace ResilienceOrg.Resilience.Domain.Domain
+{
+
+    public class JournalEntry : FullAuditedEntity<Guid>
+    {
+        public virtual Guid PersonId { get; set; }
+        public virtual Person Person { get; set; }
+        public virtual string Content { get; set; }
+        public virtual DateTime EntryDate { get; set; }
+        public virtual string Tags { get; set; }
+        public virtual bool IsPrivate { get; set; }//If user allows us to publish their diary entries 
+    }
+}

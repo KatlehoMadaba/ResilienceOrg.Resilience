0e4d27d41d98c85835d4422c8028053f2278bd5c
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-21 17:58:09 +0200

    feature:journal entry created

--- backend/src/Module/ResilienceOrg.Resilience.Domain/Domain/Journals/Journal.cs
+++ backend/src/Module/ResilienceOrg.Resilience.Domain/Domain/Journals/Journal.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Abp.Domain.Entities.Auditing;
+using ResilienceOrg.Resilience.Domain.Domain.RPersons;
 using Shesha.Domain;
 
 namespace ResilienceOrg.Resilience.Domain.Domain.Journals
@@ -11,8 +12,8 @@ namespace ResilienceOrg.Resilience.Domain.Domain.Journals
 
     public class JournalEntry : FullAuditedEntity<Guid>
     {
-        public virtual Guid PersonId { get; set; }
-        public virtual Person Person { get; set; }
+        public virtual Guid RPersonId { get; set; }
+        public virtual RPerson RPerson { get; set; }
         public virtual string Content { get; set; }
         public virtual DateTime EntryDate { get; set; }
         public virtual string Tags { get; set; }

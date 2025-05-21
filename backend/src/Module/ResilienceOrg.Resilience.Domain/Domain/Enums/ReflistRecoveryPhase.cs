13e87cbd59dea9fabdf3fd3a4b833c221194fec5
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-21 17:58:49 +0200

    feature:reflist created

---
+++ backend/src/Module/ResilienceOrg.Resilience.Domain/Domain/Enums/ReflistRecoveryPhase.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using Shesha.Domain.Attributes;
+
+namespace ResilienceOrg.Resilience.Domain.Domain.Enums
+{
+    [ReferenceList("Res", "RecoveryPhase")]
+    public enum ReflistRecoveryPhase
+    {
+        //This occurs immediately after the assault and is marked by shock,
+        //fear, anger, or numbness.
+        //Survivors may feel disoriented or struggle to process the event
+        [Description("Acute Phase")]
+        AcutePhase = 1,
+
+        //During this phase, survivors often attempt to return to normalcy,
+        //sometimes by suppressing thoughts about the incident.
+        //They may appear outwardly fine but still grapple with internal struggles
+        [Description("Outward Adjustment Phase")]
+        AdjustmentPhase = 2,
+
+        //This is when survivors begin to confront and process the trauma, 
+        //often seeking support or counseling. 
+        //Feelings of depression or anxiety may resurface,
+        //but this phase is crucial for healing
+        [Description("Integration Phase")]
+        IntegrationPhase = 3,
+
+        //In this stage, survivors work toward acceptance and rebuilding their lives. 
+        //They may redefine relationships, roles, or goals as part of their recovery.
+        [Description("Resolution Phase")]
+        ResolutionPhase = 4,
+
+        [Description("Unsure")]
+        Unsure = 5,
+    }
+}

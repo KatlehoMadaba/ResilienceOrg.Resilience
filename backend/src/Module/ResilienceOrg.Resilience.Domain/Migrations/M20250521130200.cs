a9034841842a054bb9f7edf824ff7af3e45c1f4f
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-21 18:00:17 +0200

    feature:migrations applied

---
+++ backend/src/Module/ResilienceOrg.Resilience.Domain/Migrations/M20250521130200.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace ResilienceOrg.Resilience.Domain.Migrations
+{
+    [Migration(20250521130200)]
+    public class M20250521130200 : Migration
+    {
+        public override void Up()
+        {
+            Rename.Column("Past_HasDisclosedBefore").OnTable("Res_RPersons").To("HasDisclosedBefore");
+            Rename.Column("Past_TimeElapsedInDays").OnTable("Res_RPersons").To("TimeElapsedInDays");
+            Rename.Column("Past_RecoveryPhase").OnTable("Res_RPersons").To("RecoveryPhase");
+            Rename.Column("Past_RecoveryPhaseText").OnTable("Res_RPersons").To("RecoveryPhaseText");
+        }
+
+        public override void Down()
+        {
+            Rename.Column("HasDisclosedBefore").OnTable("Res_RPersons").To("Past_HasDisclosedBefore");
+            Rename.Column("TimeElapsedInDays").OnTable("Res_RPersons").To("Past_TimeElapsedInDays");
+            Rename.Column("RecoveryPhase").OnTable("Res_RPersons").To("Past_RecoveryPhase");
+            Rename.Column("RecoveryPhaseText").OnTable("Res_RPersons").To("Past_RecoveryPhaseText");
+        }
+    }
+}
---
+++ backend/src/Module/ResilienceOrg.Resilience.Domain/Migrations/M20250521132600.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace ResilienceOrg.Resilience.Domain.Migrations
+{
+    [Migration(20250521132600)]
+    class M20250521132600 : Migration
+    {
+         
+        public override void Up()
+        {
+            Alter.Column("HasDisclosedBefore").OnTable("Res_RPersons").AsBoolean().Nullable();
+            Alter.Column("TimeElapsedInDays").OnTable("Res_RPersons").AsInt32().Nullable();
+            Alter.Column("HasReportedToAuthorities").OnTable("Res_RPersons").AsBoolean().Nullable();
+            Alter.Column("HasReceivedMedicalAttention").OnTable("Res_RPersons").AsBoolean().Nullable();
+            Alter.Column("IsVerified").OnTable("Res_RPersons").AsBoolean().Nullable();
+        }
+
+        public override void Down()
+        {
+            // Rollback to non-nullable with default values (adjust if needed)
+            Alter.Column("HasDisclosedBefore").OnTable("Res_RPersons").AsBoolean().NotNullable().WithDefaultValue(false);
+            Alter.Column("TimeElapsedInDays").OnTable("Res_RPersons").AsInt32().NotNullable().WithDefaultValue(0);
+            Alter.Column("HasReportedToAuthorities").OnTable("Res_RPersons").AsBoolean().NotNullable().WithDefaultValue(false);
+            Alter.Column("HasReceivedMedicalAttention").OnTable("Res_RPersons").AsBoolean().NotNullable().WithDefaultValue(false);
+            Alter.Column("IsVerified").OnTable("Res_RPersons").AsBoolean().NotNullable().WithDefaultValue(false);
+        }
+    }
+}

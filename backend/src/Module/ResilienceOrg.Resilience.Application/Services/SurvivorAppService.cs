032197c6c89b1068c0e544bd1f0a6c051876c884
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-05-22 17:10:56 +0200

    feature:get current person custom end point

---
+++ backend/src/Module/ResilienceOrg.Resilience.Application/Services/SurvivorAppService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Repositories;
+using Abp.Runtime.Session;
+using Abp.UI;
+using Microsoft.AspNetCore.Mvc;
+using Shesha;
+using Shesha.Domain;
+
+namespace ResilienceOrg.Resilience.Common.Services
+{
+    public class SurvivorAppService: SheshaAppServiceBase
+    {
+        private readonly IRepository<Person, Guid> _personRepository;
+
+        public SurvivorAppService(IRepository<Person, Guid> personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        public async Task<Guid> GetCurrentPersonIdAsync()
+        {
+            var userId = AbpSession.GetUserId(); // gets current user (ABP)
+            var person = await _personRepository.FirstOrDefaultAsync(p=>p.User.Id== userId);
+            if (person == null)
+            {
+                throw new UserFriendlyException("Person not found for current user.");
+            }
+
+            return person.Id;
+        }
+        [HttpGet]
+        public async Task<Guid> GetMyPersonId()
+        {
+            return await GetCurrentPersonIdAsync();
+        }
+
+    }
+}
